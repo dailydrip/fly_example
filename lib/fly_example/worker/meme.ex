dd434e59c3becb35ad0124d778fd0ca1c7b56c99
Josh Adams <josh.rubyist@gmail.com>
2016-12-18 06:41:20 -0600

    Add debug information for meme

--- lib/fly_example/worker/meme.ex
+++ lib/fly_example/worker/meme.ex
@@ -1,4 +1,6 @@
 defmodule FlyExample.Worker.Meme do
+  require Logger
+
   @moduledoc """
   Meme is a sweet Fly worker to generate a meme image.
   """
@@ -7,7 +9,7 @@ defmodule FlyExample.Worker.Meme do
   Generates an image for the provided text
   """
   def call(input, %{top: top, bottom: bottom}) do
-    %Porcelain.Result{out: input_with_top, status: 0} =
+    first =
       Porcelain.exec("convert", [
                        "-background", "none",
                        "-fill", "white",
@@ -23,26 +25,35 @@ defmodule FlyExample.Worker.Meme do
                        "-resize", "640x",
                        "-composite",
                        "-",
-                       ], [in: input, out: :string])
+                       ], [in: input, out: :string, err: :string])
 
-    %Porcelain.Result{out: output, status: 0} =
-      Porcelain.exec("convert", [
-                       "-background", "none",
-                       "-fill", "white",
-                       "-font", "Impact",
-                       "-size", "640x100",
-                       "-gravity", "center",
-                       "-stroke", "black",
-                       "-strokewidth", "3",
-                       "caption:#{String.upcase bottom}",
-                       "-",
-                       "+swap",
-                       "-gravity", "south",
-                       "-composite",
-                       "-",
-                       ], [in: input_with_top, out: :string])
-
-    output
+    case first do
+      %Porcelain.Result{out: input_with_top, status: 0} ->
+        second =
+          Porcelain.exec("convert", [
+                           "-background", "none",
+                           "-fill", "white",
+                           "-font", "Impact",
+                           "-size", "640x100",
+                           "-gravity", "center",
+                           "-stroke", "black",
+                           "-strokewidth", "3",
+                           "caption:#{String.upcase bottom}",
+                           "-",
+                           "+swap",
+                           "-gravity", "south",
+                           "-composite",
+                           "-",
+                           ], [in: input_with_top, out: :string])
+        case second do
+          %Porcelain.Result{out: output, status: 0} ->
+            output
+          %Porcelain.Result{status: _, err: err} ->
+            Logger.error err
+        end
+      %Porcelain.Result{status: _, err: err} ->
+        Logger.error err
+    end
   end
 
   defp get_temp_file do
