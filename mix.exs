db871d6769342d0f593ba000aa9d109201a3ad11
Josh Adams <josh.rubyist@gmail.com>
2016-12-18 03:48:11 -0600

    Use fly 1.4

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule FlyExample.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:fly, "~> 0.1.2"},
+      {:fly, "~> 0.1.4"},
     ]
   end
 
